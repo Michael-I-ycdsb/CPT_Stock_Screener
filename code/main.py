98c34643da81c48b037b09dc36cfdd12f9fbad7b
Michael-I-ycdsb <michael.iacobelli25@ycdsbk12.ca>
2024-04-04 13:59:48 -0400

    Created main.py and settings.py

---
+++ code/main.py
@@ -0,0 +1,48 @@
+import pygame
+from settings import *
+
+class Main():
+    def __init__(self) -> None:
+        """
+        Manages the screen and updating the program
+        """
+
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE | pygame.SCALED).convert_alpha()
+        self.clock = pygame.time.Clock()
+        self.app_state = AppState()
+
+    def run(self) -> None:
+        """
+        Runs the while loop of the code 60 times every second and updates display
+        """
+
+        while True:
+            
+            self.app_state.state_manager()
+            
+            pygame.display.update()
+            self.clock.tick(60)
+            #print(self.clock.get_fps())
+
+class AppState():
+    def __init__(self) -> None:
+        """
+        Manages the current state that needs to run
+        """
+
+        self.graph = graph()
+
+    def state_manager(self) -> None:
+        """
+        Runs which ever state is currently active
+        """
+
+        if ACTIVE_STATE['active_state'] == 'sim':
+            self.graph.run()
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
+    
+    #profile.runctx('main.run()', globals(), locals())
---
+++ code/settings.py
@@ -0,0 +1,19 @@
+import pygame
+
+WINDOW_WIDTH = (16 * 64) * 1.2
+WINDOW_HEIGHT = (9 * 64) * 1.2
+
+TILE_SIZE = 64
+
+ACTIVE_STATE = {'active_state': 'None'}
+
+TEXT_FONT = 'assets/text/PoetsenOne-Regular.ttf'
+
+KEYS_PRESSED = {
+    pygame.K_F3: False
+}
+
+
+# Colors
+
+BACKGROUND_COLOR = (50, 50, 50)
