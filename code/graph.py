e42cae828a7207996a3ffbda15a68edfc684ce04
Michaelis’700701’git config --global user.name Michaelis’700701/usr/local/bin/python3 "/Users/michaeliacobelli/Visual Studio Code Programs/Simulations/sim_3_gravity-1/code/simulation.py"git config --global user.email ‘michael52012@hotmail.com’git config --global user.name Michaelis700701’ <michael52012@hotmail.com>
2024-04-11 19:22:08 -0400

    Added draw grid lines

--- code/graph.py
+++ code/graph.py
@@ -22,6 +22,31 @@ class Graph(AppState):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, BACKGROUND_COLOR, KEYS_PRESSED)
         self.stock_name = stock_name
     
+    def draw_grid_lines(self) -> None:
+        """
+        This method draws grid lines onto this class' surface
+        TODO: make the grid lines a set size while zooming to make sure it does not change thickness
+        """
+        spacing = 15
+
+        for line_x_pos in range(int(self.origin.x), int(self.surface.get_width() + self.origin.x), spacing):
+            line_start_pos = (line_x_pos, self.origin.y)
+            line_end_pos = (line_x_pos, self.origin.y + self.surface.get_height())
+
+            pygame.draw.line(self.surface, (0, 0, 0), line_start_pos, line_end_pos)
+
+        for line_y_pos in range(int(self.origin.y), int(self.surface.get_height() + self.origin.y), spacing):
+            line_start_pos = (self.origin.x, line_y_pos)
+            line_end_pos = (self.origin.x + self.surface.get_width(), line_y_pos)
+
+            pygame.draw.line(self.surface, (0, 0, 0), line_start_pos, line_end_pos)
+
+    def draw_assets(self) -> None:
+        """
+        This method draws all assets
+        """
+        self.draw_grid_lines()
+
     def run(self) -> None:
         return super().run()
 
