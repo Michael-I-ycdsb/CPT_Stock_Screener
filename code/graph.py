17caa59e5fe5063b87cac0c5fa1319bbb98ec454
Michaelis’700701’git config --global user.name Michaelis’700701/usr/local/bin/python3 "/Users/michaeliacobelli/Visual Studio Code Programs/Simulations/sim_3_gravity-1/code/simulation.py"git config --global user.email ‘michael52012@hotmail.com’git config --global user.name Michaelis700701’ <michael52012@hotmail.com>
2024-05-09 09:25:17 -0400

    Added candles (WIP)

--- code/graph.py
+++ code/graph.py
@@ -1,4 +1,5 @@
 import pygame, yfinance
+import datetime
 from pygame.math import Vector2
 from settings import *
 from support.app_state import AppState
@@ -24,43 +25,51 @@ class Graph(AppState):
         self.side_bar = None
 
         self.text_font = pygame.font.Font(TEXT_FONT, FONT_SIZE)
+
+        self.graph_count_interval = Vector2(10, 1)
+        self.graph_spacing = Vector2(100, 60)
+        self.current_datetime = datetime.datetime.now()
     
     def draw_grid_lines(self) -> None:
         """
         This method draws grid lines onto this class' surface.
         TODO: make the grid lines a set size while zooming to make sure it does not change thickness
         """
-        spacing = Vector2(100, 55)
         origin_multiple = self.origin - Vector2(
-            x = self.origin.x - int(self.origin.x / spacing.x) * spacing.x,
-            y = self.origin.y - int(self.origin.y / spacing.y) * spacing.y
+            x = self.origin.x - int(self.origin.x / self.graph_spacing.x) * self.graph_spacing.x,
+            y = self.origin.y - int(self.origin.y / self.graph_spacing.y) * self.graph_spacing.y
         )
 
         relative_origin = self.origin - origin_multiple
 
-        for line_x_pos in range(int(relative_origin.x), int(self.surface.get_width() + relative_origin.x), int(spacing.x)):
+        for line_x_pos in range(int(relative_origin.x), int(self.surface.get_width() + relative_origin.x), int(self.graph_spacing.x)):
             line_start_pos = (line_x_pos, 0)
             line_end_pos = (line_x_pos, relative_origin.y + self.surface.get_height())
 
             pygame.draw.line(self.surface, GRAPH_LINE_COLOR, line_start_pos, line_end_pos)
 
-            number = -int(self.origin.x / spacing.x) + ((line_x_pos // spacing.x) - (relative_origin.x // spacing.x))
-            self.draw_grid_number(line_y=line_x_pos, number=number)
+            # scale for graph
+            scale_num_from_origin = int(-int(self.origin.x / self.graph_spacing.x) + ((line_x_pos // self.graph_spacing.x) - (relative_origin.x // self.graph_spacing.x)))
+            hour = str(self.current_datetime.hour + int((scale_num_from_origin * self.graph_count_interval.x) / 60))
+            minute = str(int((scale_num_from_origin * self.graph_count_interval.x) % 60)) + "0"
+            scale_num = f"{hour}:{minute[:2]}"
 
-        for line_y_pos in range(int(relative_origin.y), int(self.surface.get_height() + relative_origin.y), int(spacing.y)):
+            self.draw_grid_number(line_y=line_x_pos, number=scale_num)
+
+        for line_y_pos in range(int(relative_origin.y), int(self.surface.get_height() + relative_origin.y), int(self.graph_spacing.y)):
             line_start_pos = (0, line_y_pos)
             line_end_pos = (relative_origin.x + self.surface.get_width(), line_y_pos)
 
             pygame.draw.line(self.surface, GRAPH_LINE_COLOR, line_start_pos, line_end_pos)
 
-            number = -int(self.origin.y / spacing.y) + ((line_y_pos // spacing.y) - (relative_origin.y // spacing.y))
-            self.draw_grid_number(line_x=line_y_pos, number=number)
+            scale_num_from_origin = -int(self.origin.y / self.graph_spacing.y) + ((line_y_pos // self.graph_spacing.y) - (relative_origin.y // self.graph_spacing.y))
+            self.draw_grid_number(line_x=line_y_pos, number=str(-scale_num_from_origin * self.graph_count_interval.y))
 
     def draw_grid_number(
             self,
             line_x: float = 0,
             line_y: float = 0,
-            number: float = 0
+            number: str = "None"
     ) -> None:
         """
         Draws a grid number at the given position
@@ -71,15 +80,65 @@ class Graph(AppState):
             The x position of the line to draw the number on. Only define if the number is on the x lines.
         line_y : float = 0
             The y position of the line to draw the number on. Only define if the number is on the y lines.
-        number : float = 0
+        number : str = "None"
             The number that will be drawn.
         """
-        number_surface = self.text_font.render(str(number), True, GRAPH_LINE_NUMBER_COLOR, BACKGROUND_COLOR)
+        number_surface = self.text_font.render(number, True, GRAPH_LINE_NUMBER_COLOR, BACKGROUND_COLOR)
         number_rect = number_surface.get_rect()
         if line_x != 0: number_rect.center = Vector2(WINDOW_WIDTH - number_rect.w, line_x)
         if line_y != 0: number_rect.center = Vector2(line_y, WINDOW_HEIGHT - number_rect.h)
 
         self.surface.blit(number_surface, number_rect)
+
+    def draw_candle(
+            self,
+            candle_open: float,
+            candle_high: float,
+            candle_low: float,
+            candle_close: float,
+            time: dict["hour": int, "minute": int]
+        ) -> None:
+        #stock = yfinance.Ticker(self.stock_name)
+        
+        x_pos = (
+            (time["hour"] - self.current_datetime.hour) * (int(60 / self.graph_count_interval.x) * self.graph_spacing.x) # hour location calculation
+            + ((time["minute"]) * (self.graph_spacing.x / self.graph_count_interval.x)) # minute location calculation
+        )
+        graph_spacing_multiple = (self.graph_spacing.y / self.graph_count_interval.y)
+
+        candle_rect = pygame.Rect(0, 0,
+                int(graph_spacing_multiple) - 1,
+                int(graph_spacing_multiple * abs(candle_open - candle_close))
+            )
+
+        if candle_open < candle_close:
+            candle_rect.midbottom = self.origin + (x_pos, -candle_open * graph_spacing_multiple)
+            pygame.draw.rect(
+                self.surface,
+                GRAPH_CANDLE_BULLISH_COLOR,
+                candle_rect
+            )
+        else:
+            candle_rect.center = self.origin + (x_pos, 100)
+            pygame.draw.rect(
+                self.surface,
+                GRAPH_CANDLE_BEARISH_COLOR,
+                candle_rect
+            )
+
+    def create_candles(self, period, interval) -> None:
+        """
+        """
+        candle_datetime_list = []
+        
+        print(self.current_datetime.date())
+        stock_data = yfinance.download(tickers=self.stock_name, period=period, interval=interval)
+        datetime_stock_data = yfinance.download(tickers=self.stock_name, period=period, interval=interval)
+        datetime_stock_data.reset_index(inplace=True)
+        for datetime in datetime_stock_data["Datetime"]:
+            candle_data = stock_data.at_time(datetime)
+            print(datetime.hour, candle_data.Open.iloc[0])
+            self.draw_candle(candle_data.Open.iloc[0], candle_data.High.iloc[0], candle_data.Low.iloc[0], candle_data.Close.iloc[0], {"hour": datetime.hour, "minute": datetime.minute})
 
     def draw_side_bar(self, event) -> None:
         """
@@ -99,6 +158,8 @@ class Graph(AppState):
         This method draws all assets.
         """
         self.draw_grid_lines()
+        self.create_candles(period="1d", interval="5m")
+        #self.draw_candle(5, 25, 0, 20, {"hour": 9, "minute": 25})
     
     def draw_non_zoomable_assets(self):
         """
@@ -113,10 +174,9 @@ class Graph(AppState):
         """
         """
         self.draw_side_bar(event)
-        
+
 
     def run(self) -> None:
-        print(self.side_bar)
         return super().run()
 
 class SideBar():
